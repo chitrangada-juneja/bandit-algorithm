0f5f23ad3aa074cbb69a35188f6104d124f1b2b4
Chitrangada Juneja <chitrangada@Chitrangadas-MacBook-Air.local>
2026-02-28 22:07:37 -0700

    changed ucb

--- SequentialUCB.py
+++ SequentialUCB.py
@@ -168,7 +168,7 @@ class SequentialUCB:
                 return True, current_text, strategies_used
 
         # Attack failed
-        final_success = model(current_text) != ground_truth
+        final_success = self.model_result(current_text, ground_truth) == 1
         return final_success, current_text, strategies_used
 
 
